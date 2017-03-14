216d4edd72911d0385666b24cea93383e8a6f3ee
Santhosh <santhoshreddy121@gmail.com>
2017-03-14 11:00:24 -0400

    Update CounterGame.scala

--- src/com/scala/practice/hackerrank/CounterGame.scala
+++ src/com/scala/practice/hackerrank/CounterGame.scala
@@ -1,21 +1,22 @@
-/*Louise and Richard play a game. They have a counter set to . Louise gets the first turn and the turns alternate thereafter. In the game, they perform the following operations.
+/*Louise and Richard play a game. They have a counter set to N . Louise gets the first turn and the turns alternate thereafter. In the game, they perform the following operations.
 
-If  is not a power of , reduce the counter by the largest power of  less than .
-If  is a power of , reduce the counter by half of .
-The resultant value is the new  which is again used for subsequent operations.
-The game ends when the counter reduces to 1, i.e., , and the last person to make a valid move wins.
+If N is not a power of 2, reduce the counter by the largest power of 2 less than N.
+If N is a power of 2, reduce the counter by half of N.
+The resultant value is the new N which is again used for subsequent operations.
+The game ends when the counter reduces to 1, i.e., N==1, and the last person to make a valid move wins.
 
-Given , your task is to find the winner of the game.
+Given N, your task is to find the winner of the game.
 
-Update If they set counter to , Richard wins, because its Louise' turn and she cannot make a move.
+Update If they set counter to 1, Richard wins, because its Louise' turn and she cannot make a move.
 
 Input Format
 
-The first line contains an integer , the number of testcases. 
- lines follow. Each line contains , the initial number set in the counter.
+The first line contains an integer T, the number of testcases. 
+ T lines follow. Each line contains N, the initial number set in the counter.
 
 Constraints
-
+1<=T<=10;
+1<=N<=2**64-1
 
 
 Output Format
@@ -31,9 +32,9 @@ Sample Output
 Richard
 Explanation
 
-As  is not a power of , Louise reduces the largest power of  less than  i.e., , and hence the counter reduces to .
-As  is a power of , Richard reduces the counter by half of  i.e., . Hence the counter reduces to .
-As we reach the terminating condition with , Richard wins the game.
+As 6 is not a power of 2, Louise reduces the largest power of 2 less than 6 i.e.,4 , and hence the counter reduces to 2 .
+As  2 is a power of 2 , Richard reduces the counter by half of 2 i.e., 1 . Hence the counter reduces to 1 .
+As we reach the terminating condition with N==1, Richard wins the game.
 
 */
 
